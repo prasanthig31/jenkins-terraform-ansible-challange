608e4865722f623ef0331f039ae5db140e43f21f
prasanthi <prasanthi.g31@gmail.com>
2024-12-18 16:49:05 +0530

    adding variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "eu-central-1"
+}
+variable "key_name" {
+    default = "net"
+}
+variable "ami_linux" {
+    default = "ami-0e54671bdf3c8ed8d"
+}
+variable instance_type {
+    default = "t2.large"
+ }
+ variable "ami_ubuntu" {
+    default = "ami-0a628e1e89aaedf80"
+ }
