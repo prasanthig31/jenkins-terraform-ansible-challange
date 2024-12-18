c2f6984c724783f3225734c9eae84760ad693d58
prasanthi <prasanthi.g31@gmail.com>
2024-12-18 16:13:05 +0530

    jenkins-terraform-ansible-challenge

---
+++ main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+    region = var.region
+}
+
+# Amazon Linux Instance
+resource "aws_instance" "amazon_linux_vm" {
+  ami = var.ami_linux 
+  instance_type = var.instance_type
+  key_name = var.key_name
+
+  tags = {
+    Name = "c8.local" # Tag for the instance
+  }
+  # User Data to Set the Hostname
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname c8.local
+              echo "127.0.0.1 c8.local" >> /etc/hosts
+              EOF
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "[frontend]" >> inventory.yml
+      echo "c8.local" >> inventory.yml
+    EOT
+  }
+}
+
+# Ubuntu Instance
+resource "aws_instance" "ubuntu_vm" {
+  ami = var.ami_ubuntu
+  instance_type = var.instance_type
+  key_name = var.key_name
+
+  tags = {
+    Name = "u21.local"
+  }
+  # User Data to Set the Hostname
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname u21.local
+              echo "127.0.0.1 u21.local" >> /etc/hosts
+              EOF
+    provisioner "local-exec" {
+    command = <<EOT
+      echo "[backend]" >> inventory.yml
+      echo "u21.local" >> inventory.yml
+    EOT
+  }
+}
+
+
+
+# Output: Public IPs
+output "instance_public_ips" {
+  value = [
+    aws_instance.amazon_linux_vm.public_ip,
+    aws_instance.ubuntu_vm.public_ip
+  ]
+  description = "Public IPs of Amazon Linux and Ubuntu VMs"
+}
+
