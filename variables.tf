f4af32abc5ae5a96afeb62ed0f3a73f165edeadb
prasanthig31 <142896999+prasanthig31@users.noreply.github.com>
2024-12-20 13:30:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
-    default = "eu-central-1"
+    default = "ap-south-1"
 }
 variable "key_name" {
     default = "net"
 }
 variable "ami_linux" {
-    default = "ami-0e54671bdf3c8ed8d"
+    default = "ami-0327f51db613d7bd2"
 }
 variable instance_type {
     default = "t2.large"
  }
  variable "ami_ubuntu" {
-    default = "ami-0745b7d4092315796"
+    default = "ami-09b0a86a2c84101e1"
  }
